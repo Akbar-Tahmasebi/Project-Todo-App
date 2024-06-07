a93061fe8b737414c67ece1cf8708230e0c041cc
erfan <pahnab021@gmail.com>
2024-06-07 15:29:57 +0330

    Add all folders

---
+++ scripts/scrips.js
@@ -0,0 +1,18 @@
+let myForm = document.querySelector('#myform');
+let textTodo = document.querySelector('.todo');
+let total = 0;
+
+myForm.addEventListener('submit', function(){
+    if(textTodo.value != null){
+        let itme = document.createElement('li')
+        itme.innerHTML = textTodo.value;
+        textTodo.value = null;
+        let ulItme = document.querySelector('#tasks ul');
+        ulItme.appendChild(itme);
+        total++;
+        document.querySelector('#totalnumber').innerHTML = total;
+
+    }else{
+        alert('type something ...');
+    } 
+});
